ae52e5653415dadcca8c6e6054c39403ddc58976
Meg Grasse <meggrasse@gmail.com>
2016-12-29 12:55:42 -0500

    Refactored, connected to iTunes, fixed some bugs.

--- dj.py
+++ dj.py
@@ -1,74 +1,62 @@
-import pyglet
 import time
 import requests
 import json
-import csv
 import random
 import math
+import urllib
+import vlc
+import copy
 
-music_corpus = {}
-#open music csv file containing the song names and genres.
-#using variables, split the genres into one variable genre and the song
-#titles in variable filepath
-#add all the genres into music_corpus
-with open('music.csv', 'rU') as musicfile:
-    musicreader = csv.reader(musicfile, dialect=csv.excel_tab)
-    for row in musicreader:
-        genre = row[0].split(',')[1]
-        filepath = row[0].split(',')[0]
-        if genre not in music_corpus:
-            music_corpus[genre] = [filepath]
-        else:
-            music_corpus[genre].append(filepath)
-player = pyglet.media.Player()
+chart_data_dict = {}
+genre_dict = {'country' : 6, 'electronic' : 7, 'pop' : 8, 'dance' : 17, 'rap' : 18, 'rock' : 21}
 
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+# updates the track data stored in the dict
+def update_track_data():
+    for key, val in genre_dict.iteritems():
+        chart_data_dict[key] = json.loads(urllib.urlopen('https://itunes.apple.com/us/rss/topsongs/limit=10/genre=' + str(val) + '/json').read())
+
+# clear votes
 def clear_vote_count():
-    r = requests.get('http://smartclub.herokuapp.com/clearvotes')
+    vote_res = requests.get('http://smartclub.herokuapp.com/clearvotes')
+    scream_res = requests.get('http://smartclub.herokuapp.com/resetscreamtracker')
 
-def play(type):
+# randomly select a new genre
+def switch_genre(genre):
+    temp_genre_dict = copy.deepcopy(genre_dict)
+    temp_genre_dict.pop(genre)
+    random_index = int(math.floor(random.random() * len(temp_genre_dict)))
+    return play(temp_genre_dict.keys()[random_index])
+
+def update_crowd_feedback():
+    vote_res = requests.get('http://smartclub.herokuapp.com/getvotecount')
+    scream_res = requests.get('http://smartclub.herokuapp.com/wasthereascream')
+    return (json.loads(vote_res.text), scream_res)
+
+def play(genre):
     clear_vote_count()
-
-    number_of_songs_in_type = len(music_corpus[type])
-    random_index = int(math.floor(random.random() * number_of_songs_in_type))
-    song = pyglet.resource.media(music_corpus[type][random_index])
-    player.queue(song)
-    player.next_source()
-
-    player.volume = 1.0
-    while 1:
-        try:
-            player.play()
-            time.sleep(10)
-            r = requests.get('http://smartclub.herokuapp.com/getvotecount')
-            votes = json.loads(r.text)
-            if (votes['upvotes'] > votes['downvotes']):
-                return play(type)
-            else:
-                other_types = [key for key, val in music_corpus.iteritems()]
-                other_types.remove(type)
-                random_index = int(math.floor(random.random() * len(other_types)))
-                return play(other_types[random_index])
-        except KeyboardInterrupt:
-            print "interrupt"
-            break
-
-def start_dj(type):
-    clear_vote_count()
-    number_of_songs_in_type = len(music_corpus[type])
-    random_index = int(math.floor(random.random() * number_of_songs_in_type))
-    song = pyglet.resource.media(music_corpus[type][random_index])
-    player.queue(song)
+    top_track_list = chart_data_dict[genre]['feed']['entry']
+    random_index = int(math.floor(random.random() * len(top_track_list)))
+    song_url = top_track_list[random_index]['link'][1]['attributes']['href']
+    media = instance.media_new(song_url)
+    player.set_media(media)
     player.play()
-    time.sleep(10)
-    r = requests.get('http://smartclub.herokuapp.com/getvotecount')
-    votes = json.loads(r.text)
-    if (votes['upvotes'] > votes['downvotes']):
-        play(type)
-    else:
-        other_types = [key for key, val in music_corpus.iteritems()]
-        other_types.remove(type)
-        random_index = int(math.floor(random.random() * len(other_types)))
-        play(other_types[random_index])
+    while 1:
+        time.sleep(10)
+        # after 10 seconds, get request to check upvotes/downvotes and possible noise reaction
+        votes, scream_res = update_crowd_feedback()
+        if ((votes['upvotes'] > votes['downvotes']) or (scream_res.text == "Yes")):
+            # play the rest of the song (20 seconds since its a preview url)
+            time.sleep(20)
+            votes, scream_res = update_crowd_feedback()
+            # if they still like the song play a similar one
+            if ((votes['upvotes'] > votes['downvotes']) or (scream_res.text == "Yes")):
+                return play(genre)
+            else:
+                return switch_genre(genre)
+        else:
+            # if they don't like it, switch to a different song
+           return switch_genre(genre)
 
-start_dj("pop")
-sys.exit()
